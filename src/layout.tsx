b05a2d6ca29eec18aa170d78c83baeb94b58c1d7
Riad Zaid <62952229+RiadZX@users.noreply.github.com>
2025-02-01 22:52:30 +0100

    added vercel analytics

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css"
 import type { Metadata } from "next"
 import type React from "react"
+import { Analytics } from "@vercel/analytics/react"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
